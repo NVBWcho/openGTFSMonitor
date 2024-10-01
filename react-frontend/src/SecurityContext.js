5dfc7f2910f257f69048b1858968251d557b6b9c
NVBWCho <Kangkan.Choudhury@nvbw.de>
2024-10-01 11:58:32 +0200

    integrating frontend

--- react-frontend/src/SecurityContext.js
+++ react-frontend/src/SecurityContext.js
@@ -1,14 +1,6 @@
 export const currentContext='https';
 
-export const userapiContext='https://bfrk-kat-api.efa-bw.de/gtfsapi'
 
-export const plzService="https://bfrk-kat-api.efa-bw.de/plzapi"
-
-// export const aufzugHistoryService="http://10.70.186.71/fastaAnalyse"
-
-export const aufzugHistoryService="https://bfrk-kat-api.efa-bw.de/fastaAnalyse"
-
-export const gtfsService="https://bfrk-kat-api.efa-bw.de/gtfsapi/gtfsAnalyse";
 
 export const applicationBackendService="http://localhost:3001/gtfsAnalyse"
 
